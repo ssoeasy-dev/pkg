86906987934916b7c55e0cf7f1c813928f68de74
MoreWiktor <morewiktor@yandex.ru>
2026-02-28 15:00:47 +0300

    fix(db): lint

--- db/tx/mock.go
+++ db/tx/mock.go
@@ -3,16 +3,20 @@ package tx
 import (
 	"context"
 
+	"github.com/ssoeasy-dev/pkg/logger"
 	"github.com/stretchr/testify/mock"
 	"gorm.io/gorm"
 )
 
 type MockTxManager struct {
 	mock.Mock
+	log *logger.Logger
 }
 
-func NewMockTxManager() *MockTxManager {
-	return &MockTxManager{}
+func NewMockTxManager(log *logger.Logger) *MockTxManager {
+	return &MockTxManager{
+		log: log,
+	}
 }
 
 func (m *MockTxManager) Begin(ctx context.Context) (context.Context, error) {
@@ -51,14 +55,24 @@ func (m *MockTxManager) WithTransaction(ctx context.Context, fn func(ctx context
 func (m *MockTxManager) WithTransactionalSuccess(ctx context.Context) {
 	m.On("WithTransaction", ctx, mock.AnythingOfType("func(context.Context) error")).Run(func(args mock.Arguments) {
 		fn := args.Get(1).(func(context.Context) error)
-		fn(ctx)
+		err := fn(ctx)
+		if err != nil {
+			m.log.Error(ctx, "WithTransactionalSuccess Function Error", map[string]any{
+				"error": err.Error(),
+			})
+		}
 	}).Return(nil)
 }
 
 func (m *MockTxManager) WithTransactionalRollback(ctx context.Context, err error) {
 	m.On("WithTransaction", ctx, mock.AnythingOfType("func(context.Context) error")).Run(func(args mock.Arguments) {
 		fn := args.Get(1).(func(context.Context) error)
-		fn(ctx)
+		err := fn(ctx)
+		if err != nil {
+			m.log.Error(ctx, "WithTransactionalRollback Function Error", map[string]any{
+				"error": err.Error(),
+			})
+		}
 	}).Return(err)
 }
 
@@ -69,13 +83,23 @@ func (m *MockTxManager) WithTransactionErrBegin(ctx context.Context) {
 func (m *MockTxManager) WithTransactionErrCommit(ctx context.Context) {
 	m.On("WithTransaction", ctx, mock.AnythingOfType("func(context.Context) error")).Run(func(args mock.Arguments) {
 		fn := args.Get(1).(func(context.Context) error)
-		fn(ctx)
+		err := fn(ctx)
+		if err != nil {
+			m.log.Error(ctx, "WithTransactionErrCommit Function Error", map[string]any{
+				"error": err.Error(),
+			})
+		}
 	}).Return(ErrTxCommit)
 }
 
 func (m *MockTxManager) WithTransactionErrRollback(ctx context.Context) {
 	m.On("WithTransaction", ctx, mock.AnythingOfType("func(context.Context) error")).Run(func(args mock.Arguments) {
 		fn := args.Get(1).(func(context.Context) error)
-		fn(ctx)
+		err := fn(ctx)
+		if err != nil {
+			m.log.Error(ctx, "WithTransactionErrRollback Function Error", map[string]any{
+				"error": err.Error(),
+			})
+		}
 	}).Return(ErrTxRollback)
 }

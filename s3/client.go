16a984e795a0ed558ac6fd79da06bdb2a8c06b75
MoreWiktor <morewiktor@yandex.ru>
2026-03-11 18:40:36 +0300

    feat: s3

--- s3/client.go
+++ s3/client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	awsconfig "github.com/aws/aws-sdk-go-v2/config"
@@ -14,7 +15,7 @@ import (
 // Client — generic S3 клиент поверх aws-sdk-go-v2.
 // Работает с любым S3-совместимым хранилищем (AWS, Tinkoff Cloud, MinIO и т.д.).
 type Client struct {
-	s3     *s3.Client
+	S3     *s3.Client
 	bucket string
 }
 
@@ -39,52 +40,112 @@ func NewClient(cfg *Config) (*Client, error) {
 		if cfg.Endpoint != "" {
 			o.BaseEndpoint = aws.String(cfg.Endpoint)
 		}
+		o.ResponseChecksumValidation = aws.ResponseChecksumValidationUnset
 	})
 
 	return &Client{
-		s3:     client,
+		S3:     client,
 		bucket: cfg.Bucket,
 	}, nil
 }
 
+type Object struct {
+	Key           string
+	ContentType   *string
+	ContentLength *int64
+	Range         *string
+	ETag          *string
+}
+
 // Put загружает объект по ключу key с указанным contentType.
-func (c *Client) Put(ctx context.Context, key string, body io.Reader, contentType string) error {
-	_, err := c.s3.PutObject(ctx, &s3.PutObjectInput{
+func (c *Client) Put(ctx context.Context, file io.Reader, obj *Object) error {
+	if file == nil {
+		return fmt.Errorf("Error")
+	}
+	if obj == nil {
+		return fmt.Errorf("Error")
+	}
+
+	o, err := c.S3.PutObject(ctx, &s3.PutObjectInput{
 		Bucket:      aws.String(c.bucket),
-		Key:         aws.String(key),
-		Body:        body,
-		ContentType: aws.String(contentType),
+		Key:         aws.String(obj.Key),
+		Body:        file,
+		ContentType: obj.ContentType,
 	})
+
+	obj.ETag = o.ETag
+
 	if err != nil {
-		return fmt.Errorf("s3: put %q: %w", key, err)
+		return fmt.Errorf("s3: put %q: %w", obj.Key, err)
 	}
 	return nil
+}
+
+type ListResult struct {
+	Key       string
+	Size      int64
+	ETag      string
+	UpdatedAt time.Time
+}
+
+// List возвращает объекты по ключу key.
+func (c *Client) List(ctx context.Context, key string) ([]ListResult, error) {
+	var continuationToken *string
+	var res []ListResult
+	for {
+		listOut, err := c.S3.ListObjectsV2(ctx, &s3.ListObjectsV2Input{
+			Bucket:            aws.String(c.bucket),
+			Prefix:            aws.String(key),
+			ContinuationToken: continuationToken,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		for _, obj := range listOut.Contents {
+			res = append(res, ListResult{
+				Key:       aws.ToString(obj.Key),
+				Size:      aws.ToInt64(obj.Size),
+				ETag:      aws.ToString(obj.ETag),
+				UpdatedAt: aws.ToTime(obj.LastModified),
+			})
+		}
+
+		if !aws.ToBool(listOut.IsTruncated) {
+			break
+		}
+		continuationToken = listOut.NextContinuationToken
+	}
+
+	return res, nil
 }
 
 // Get возвращает объект по ключу key.
 // rangeHeader — значение HTTP-заголовка Range (например "bytes=0-1023").
 // Передайте пустую строку для получения полного объекта.
 // Вызывающий обязан закрыть Body у возвращённого объекта.
-func (c *Client) Get(ctx context.Context, key string, rangeHeader string) (*s3.GetObjectOutput, error) {
-	input := &s3.GetObjectInput{
+func (c *Client) Get(ctx context.Context, obj *Object) (*io.ReadCloser, error) {
+	out, err := c.S3.GetObject(ctx, &s3.GetObjectInput{
 		Bucket: aws.String(c.bucket),
-		Key:    aws.String(key),
-	}
-	if rangeHeader != "" {
-		input.Range = aws.String(rangeHeader)
+		Key:    aws.String(obj.Key),
+		Range:  obj.Range,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("s3: get %q: %w", obj.Key, err)
 	}
 
-	out, err := c.s3.GetObject(ctx, input)
-	if err != nil {
-		return nil, fmt.Errorf("s3: get %q: %w", key, err)
-	}
-	return out, nil
+	obj.ETag = out.ETag
+	obj.Range = out.ContentRange
+	obj.ContentType = out.ContentType
+	obj.ContentLength = out.ContentLength
+
+	return &out.Body, nil
 }
 
 // Head возвращает метаданные объекта без скачивания тела.
 // Удобен для получения Content-Length, Content-Type, ETag.
 func (c *Client) Head(ctx context.Context, key string) (*s3.HeadObjectOutput, error) {
-	out, err := c.s3.HeadObject(ctx, &s3.HeadObjectInput{
+	out, err := c.S3.HeadObject(ctx, &s3.HeadObjectInput{
 		Bucket: aws.String(c.bucket),
 		Key:    aws.String(key),
 	})
